9257e077e7c0697c07ab665948c2a54c75bc466c
Geunha.Yi <yi.gexnha@gmail.com>
2025-11-07 19:02:26 +0900

    edit e-03-binding

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <E02Reactive />
+  <div>
+    <input v-model="message" />
+    <p>{{ message }}</p>
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import E02Reactive from "@/components/example1/E-02-reactive.vue";
+<script setup lang="ts">
+import { ref } from "vue";
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    E02Reactive
-  }
-});
+const message = ref("Hello Vue");
 </script>
-
-<style>
-</style>
